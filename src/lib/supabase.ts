593b0b5073c12c158e315855c12928b27f8dd39d
hammad-dc <hammaddc3@gmail.com>
2026-03-07 15:57:52 +0530

    did minor changes in the ui

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,10 @@
 import { AppState } from 'react-native';
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';import { SUPABASE_URL, SUPABASE_ANON_KEY } from '../keys'; // Direct import
 
-const supabaseUrl = 'https://lvxqrroiiuyxtygvlvib.supabase.co'; // Paste URL here
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2eHFycm9paXV5eHR5Z3ZsdmliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1OTg1NTIsImV4cCI6MjA4MDE3NDU1Mn0.xbHWlDj1ZcHbtPduH0CEmNrHLrz9DSTWz3koF2Lj52c'; // Paste Key here
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
